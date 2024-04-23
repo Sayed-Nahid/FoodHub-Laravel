2b3b9082668243cfe5bde44da44e5c382591563d
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-04-23 12:14:31 +0600

    First Commit and Laravel is welled connected

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\MenuController;
+use App\Http\Controllers\Frontend\BooktableController;
+use App\Http\Controllers\Frontend\AboutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/menu', [MenuController::class, 'index']);
+Route::get('/book', [BooktableController::class, 'index']);
